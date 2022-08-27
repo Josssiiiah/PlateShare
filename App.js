2a1753d49e9399bed4eb9bf194f0d86433a3783a
Josssiiiah <102438546+Josssiiiah@users.noreply.github.com>
2022-08-27 10:45:44 -0500

    Installed Tailwind and Expo

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import React from "react";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>np App.js to stapp!</Text>
+    <View className = "flex-1 items-center justify-center text-[#00FFFF]">
+      <Text className= "text-black">np App.js to stap!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
